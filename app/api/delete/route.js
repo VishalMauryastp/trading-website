750afacd5665b6cee39c352dde03feb9829e5254
Vishal Maurya <vishalmaurya12nov@gmil.com>
2024-04-16 15:25:32 +0530

    fixed all left to map

--- app/api/delete/route.js
+++ app/api/delete/route.js
@@ -1,6 +1,32 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function POST() {
+// export async function POST() {
+//   const dirRelativeToPublicFolder = "assets";
+//   const publicDir = path.join(
+//     process.cwd(),
+//     "public",
+//     dirRelativeToPublicFolder
+//   );
+
+//   try {
+//     const filenames = fs.readdirSync(publicDir);
+
+//     filenames.forEach((filename) => {
+//       const filePath = path.join(publicDir, filename);
+//       fs.unlinkSync(filePath); // Delete the file
+//     });
+
+//     NextResponse.json({ message: "Files deleted successfully" });
+//   } catch (error) {
+//     console.error("Error deleting files:", error);
+//     NextResponse.json({ error: "Internal Server Error" });
+//   }
+// }
+import { NextResponse } from "next/server";
+import path from "path";
+import fs from "fs";
+
+export async function POST(req, res) {
   const dirRelativeToPublicFolder = "assets";
   const publicDir = path.join(
     process.cwd(),
@@ -9,16 +35,27 @@ export async function POST() {
   );
 
   try {
-    const filenames = fs.readdirSync(publicDir);
+    const reqbody = await req.json();
+    const { fileName } = reqbody;
+    console.log(fileName);
 
-    filenames.forEach((filename) => {
-      const filePath = path.join(publicDir, filename);
+    const filePath = path.join(publicDir, fileName);
+
+    if (fs.existsSync(filePath)) {
       fs.unlinkSync(filePath); // Delete the file
-    });
-
-    NextResponse.json({ message: "Files deleted successfully" });
+      return NextResponse.json({ message: `${fileName} deleted successfully` });
+    } else {
+      return NextResponse.json(
+        { error: `${fileName} not found` },
+        { status: 404 }
+      );
+    }
+    // return NextResponse.json({ message: "seccess" });
   } catch (error) {
-    console.error("Error deleting files:", error);
-    NextResponse.json({ error: "Internal Server Error" });
+    console.error("Error deleting file:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
--- app/upload/page.jsx
+++ app/upload/page.jsx
@@ -1,10 +1,13 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
+import { VscFilePdf } from "react-icons/vsc";
 
 const Page = () => {
   const router = useRouter();
   const [selectedFile, setSelectedFile] = useState(null);
+  const [allFiles, setAllFiles] = useState([]);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -18,26 +21,45 @@ const Page = () => {
 
     const response = await fetch("/api/upload", requestOptions);
     const result = await response.text();
-    if (result.status == "success") {
-      setSelectedFile(null);
-    }
+    // if (result.status == "success") {
+    //   getPublicAssets();
+    // }
+    getPublicAssets();
     console.log(result);
   };
 
-  // const getPublicAssets = async () => {
-  //   try {
-  //     const response = await fetch("/api/upload", { method: "GET" });
+  const getPublicAssets = async () => {
+    try {
+      const response = await axios.get("/api/upload");
+      return setAllFiles(response?.data?.files);
+    } catch (error) {
+      console.log("Error fetching public assets:", error.message);
+      return null;
+    }
+  };
 
-  //     console.log(response);
+  const deleteFile = async (fileName) => {
+    await axios({
+      method: "post",
+      url: "/api/delete",
+      data: { fileName: fileName },
+    })
+      .then((res) => {
+        if (res?.data?.error) {
+          return console.error(res?.data?.error);
+        }
+        getPublicAssets();
 
-  //     return data;
-  //   } catch (error) {
-  //     console.log("Error fetching public assets:", error.message);
-  //     return null;
-  //   }
-  // };
+        return console.log(res?.data?.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  // getPublicAssets();
+  useEffect(() => {
+    getPublicAssets();
+  }, []);
 
   return (
     <div className="  w-[90%] mx-auto  py-20">
@@ -56,11 +78,123 @@ const Page = () => {
           Upload File
         </button>
       </div>
-      <div>
-        <h1>Uploaded Files</h1>
+      <div className="py-20">
+        <h1 className="text-3xl text-center font-bold  ">Uploaded Files</h1>
+
+        <div className="mt-16 overflow-x-auto">
+          <FileListView deleteFile={deleteFile} files={allFiles} />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Page;
+
+const getFileType = (filePath) => {
+  return filePath.split(".").pop();
+};
+
+const FileView = ({ deleteFile, fileName, filePath }) => {
+  const fileType = getFileType(filePath);
+
+  // const deleteFile = async (fileName) => {
+  //   await axios({
+  //     method: "post",
+  //     url: "/api/delete",
+  //     data: { fileName: fileName },
+  //   })
+  //     .then((res) => {
+  //       if (res?.data?.error) {
+  //         return console.error(res?.data?.error);
+  //       }
+  //       getPublicAssets();
+
+  //       return console.log(res?.data?.message);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  return (
+    <tr className=" border">
+      <td className="p-4">
+        {fileType === "pdf" ? (
+          <div>
+            {/* <embed
+              className=""
+              src={filePath}
+              type="application/pdf"
+              width="50"
+              height="50"
+            /> */}
+            <VscFilePdf className="text-5xl" />
+            {/* <p>{filePath} </p> */}
+          </div>
+        ) : (
+          <img
+            className=""
+            src={filePath}
+            alt="file"
+            style={{ width: "50px", height: "auto" }}
+          />
+        )}
+      </td>
+      <td className="px-4 border">{filePath.replace(/^\\assets\\/, "")}</td>
+      <td className="px-4 border">
+        <a
+          target="_blank"
+          href={`http://localhost:3000/${filePath}`}
+          className=""
+        >
+          Preview
+        </a>
+      </td>
+      <td className="px-4 border">
+        <button
+          onClick={() => {
+            deleteFile(filePath.replace(/^\\assets\\/, ""));
+          }}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+const FileListView = ({ deleteFile, files }) => {
+  return (
+    <>
+      {files.length > 0 && (
+        <table className="table-fixed mx-auto">
+          <thead>
+            <tr className="border">
+              <th className="border px-4 py-4 bg-blue-400">Files</th>
+              <th className="border px-4 py-4 bg-blue-400">Files Name</th>
+              <th className="border px-4 py-4 bg-blue-400">Preview</th>
+              <th className="border px-4 py-4 bg-blue-400">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {files.map((filePath, index) => (
+              <FileView
+                key={index}
+                fileName={`File ${index + 1}`}
+                deleteFile={deleteFile}
+                filePath={filePath}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
+      {files.length == 0 && (
+        <h1 className="text-center text-2xl font-semibold">
+          {" "}
+          Files Not Found /Error fetching
+        </h1>
+      )}
+    </>
+  );
+};
